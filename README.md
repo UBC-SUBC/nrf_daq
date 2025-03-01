e0e9306527fcdab8eb0a44910d0be96548faa8b5
joce04 <jocelyn.zhao@smus.ca>
2025-03-01 11:20:09 -0800

    added some readme and created main.c outline

---
+++ README.md
@@ -0,0 +1,22 @@
+This is a "broken" version of the i2c code for the hall-effect sensor. It is for the MLX90393-SLQ-ABA-011-RE hall effect sensor with the datasheet here: [Datasheet](https://www.digikey.ca/en/products/detail/melexis-technologies-nv/MLX90393SLQ-ABA-011-RE/11506923)
+
+### How to run
+
+Here is the pin mapping for the nrf52840dk board.
+
+![pin layout on nrfdk52840 board](images/pinmapping-1.png "PinMapping")
+
+Connect the following pins together
+
+| Pin Name | NRF52840 Pin |
+|----------|--------------|
+| GND      | GND          |
+| SCL      | P27          |
+| SDA      | P26          |
+| MS       | VDD          |
+| INT      | GND (optional)  |
+| VDD      | VDD          |
+
+Connect to the terminal VCOM0
+
+A demo application exists at [Melexis Driver for mls90394](https://github.com/zephyrproject-rtos/zephyr/tree/v4.1.0-rc3/drivers/sensor/melexis)
--- src/main.c
+++ src/main.c
@@ -7,75 +7,73 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
-/* STEP 3 - Include the header file of the I2C API */
 #include <zephyr/drivers/i2c.h>
 
-/* STEP 4.1 - Include the header file of printk() */
 #include <zephyr/sys/printk.h>
-#include <zephyr/sys/byteorder.h>
 
-/* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS 1000
 
-/* STEP 8 - Define the I2C slave device address and the addresses of relevant registers */
+// MLX90393-SLQ-ABA-011-RE 
+#define CONFIG_REG      0x0C
+#define CONFIG_VAL      0x16
+#define RM_REG          0x4F
+#define I2C_ADDR        0x0C
 
-
-/* STEP 6 - Get the node identifier of the sensor */
-#define I2C0_NODE DT_NODELABEL(temp_sensor)
+#define I2C0_NODE DT_NODELABEL(mag_sensor)
 
 int main(void)
 {
 
     int ret;
 
-    /* STEP 7 - Retrieve the API-specific device structure and make sure that the device is
-    * ready to use  */
-    printk("I2C sensor application\n");
+    printk("Hall effect sensor application\n");
     static const struct i2c_dt_spec dev_i2c = I2C_DT_SPEC_GET(I2C0_NODE);
+
     if (!device_is_ready(dev_i2c.bus)) {
         printk("I2C device not ready\n");
         return -1;
     } else {
         printk("I2C device ready\n");
+        printk("I2C bus: %s, Address: 0x%X\n", dev_i2c.bus->name, dev_i2c.addr);
     }
 
-    uint8_t i2c_addr[1] = {0x44};
-    
-    ret = i2c_write_dt(&dev_i2c, i2c_addr, 1);
+    // set up exit
+    uint8_t exit[1] = {0x80};
+    ret = i2c_write_dt(&dev_i2c, exit, sizeof(exit));
     if(ret != 0){
-        printk("Failed to write to I2C device address %x", dev_i2c.addr);
-        return -1;
+        printk("Failed to write to I2C device address %x\n", dev_i2c.addr);
+    } else {
+        printk("Exit set\n");
     }
 
-    uint8_t config[2];
-    sys_put_be16(0x2400, config);
+    uint8_t config[2] = {CONFIG_REG, CONFIG_VAL};
+
     ret = i2c_write_dt(&dev_i2c, config, sizeof(config));
     if(ret != 0){
-        printk("Failed to write to I2C device address %x", dev_i2c.addr);
+        printk("Failed to write to I2C device address %x\n", dev_i2c.addr);
         return -1;
     }
-    while (1) {
-        /* STEP 10 - Read the temperature from the sensor */
-        
-        uint8_t read_buf[6];
-        ret = i2c_write_read_dt(&dev_i2c, i2c_addr, 1, read_buf, 6);
-        if(ret != 0){
-            printk("Failed to write/read I2C device address %x for temperature or humidity", dev_i2c.addr);
-        }
 
-        /* STEP 11 - Convert the two bytes to a 16-bits */
-        uint16_t temp = (read_buf[0] << 8) | read_buf[1];
-        uint16_t humidity = (read_buf[3] << 8) | read_buf[4];
 
-        // Convert to engineering units 
-        double cTemp = temp * 0.0625;
-        double fTemp = cTemp * 1.8 + 32;
+    while (1) {        
+        uint8_t read_buf[8];
+        uint8_t read_reg[1] = {RM_REG};
+        ret = i2c_write_read_dt(&dev_i2c, read_reg, 2, read_buf, 8);
+        if (ret != 0) {
+			printk("Failed to write/read I2C device address %x at Reg. %x \n",
+			       dev_i2c.addr, read_reg[0]);
+		}
+
+        uint8_t t = (read_buf[0] << 8) | read_buf[1];
+        uint8_t x = (read_buf[2] << 8) | read_buf[3];
+        uint8_t y = (read_buf[4] << 8) | read_buf[5];
+        uint8_t z = (read_buf[6] << 8) | read_buf[7];
 
         //Print reading to console  
-        printk("Temperature in Celsius : %.2f C \n", cTemp);
-        printk("Temperature in Fahrenheit : %.2f F \n", fTemp);
-
-        printk("Humidity : %d %% \n", humidity);
+        printk("t: %d\n", t);
+        printk("x: %d\n", x);
+        printk("y: %d\n", y);
+        printk("z: %d\n", z);
 
         k_msleep(SLEEP_TIME_MS);
     }
