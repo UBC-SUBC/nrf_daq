8b0263feafdcd28d776a610d4efa1dd346587a00
joce04 <jocelyn.zhao@smus.ca>
2025-03-08 11:48:30 -0800

    modified reg values

--- src/reg.h
+++ src/reg.h
@@ -2,7 +2,9 @@
 // MLX90393-SLQ-ABA-011-RE 
 
 #define CONFIG_REG      0x10
-#define CONFIG_VAL      0x16
-#define RM_REG          0x4F
-#define I2C_ADDR        0x10
+// #define CONFIG_VAL      0x16
+// #define RM_REG          0x4F
+#define RST_REG         0xF0
+#define EX_REG          0x80
+
 #define SLEEP_TIME_MS   1000
