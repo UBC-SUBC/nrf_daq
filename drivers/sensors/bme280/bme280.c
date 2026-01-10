864b87b37f6f75f7b87af8fc6663764edb77a14e
klemos06 <k.lemos0504@gmail.com>
2026-01-10 12:30:56 -0800

    Modified bme functions

--- drivers/sensors/bme280/bme280.c
+++ drivers/sensors/bme280/bme280.c
@@ -14,8 +14,18 @@ int setup_bme280() {
 
     // finish the init setups for the BME280
 
+    uint8_t config[2] = {BME_CONFIG_ADDRESS, 0x48};
+
+    int return_val = i2c_write_dt(&i2c_dev, config, sizeof(config));
+    
+    if (return_val != 0){
+        printk ("BME not connected")
+        return -1
+    }
+    else {
     printk("BME280 setup complete\n");
-    return 0;
+        return 0;
+    }
 }
 
 /**
@@ -27,9 +37,30 @@ int setup_bme280() {
  */
 int read_temperature_celsius(double *temperature) {
     // finish reading temperature from BME280 over I2C
+    uint32_t calc_val1;
+    uint32_t calc_val2; 
+    uint32_t temp_read = {0}; 
+    uint8_t sensor_register = BME_I2C_ADDRESS; 
 
-    *temperature = 25.0; // placeholder value
-    return 0;
+    int readcheck = i2c_write_read_dt(&i2c_dev, &sensor_register, 3, &temp_read, 3);
+
+    if (readcheck != 0) {
+        printk("Temperature unsuccessfully read");
+        return -1;
+    }
+
+    temp_read = temp_read >> 4; 
+
+    calc_val1 =(uint32_t)((temp_read/8) - ((uint32_t)BME_DIG_T1*2));
+    calc_val1 = (calc_val1 * ((uint32_t)BME_DIG_T2))/ 2048;
+    calc_val2 =(uint32_t)((temp_read/16) - ((uint32_t)BME_DIG_T1*2));
+    calc_val2 = (((calc_val2 * calc_val2)/ 4096)* ((uint32_t)BME_DIG_T3)) / 16384; 
+
+    uint32_t throwaway = calc_val1 + calc_val2;
+    uint32_t final_temp = (float)((throwaway * 5 +128)/256);
+
+    *temperature = final_temp; 
+    return 0; 
 }
 
 /**
@@ -43,7 +74,7 @@ int read_temperature_fahrenheit(double *temperature) {
     double celsius;
     int ret = read_temperature_celsius(&celsius);
     if (ret != 0) {
-        return ret;
+        return -1;
     }
     *temperature = celsius * 1.8 + 32;
     return 0;
--- drivers/sensors/bme280/bme280.h
+++ drivers/sensors/bme280/bme280.h
@@ -1,3 +1,10 @@
+#define BME_CONFIG_ADDRESS 0xF5
+#define BME_I2C_ADDRESS 0x77
+#define BME_DIG_T1 0x88
+#define BME_DIG_T2 0x8A
+#define BME_DIG_T3 0x8C
+
+
 /**
  * TODO: Complete the BME280 setup function
  * 
@@ -20,3 +27,5 @@ int read_temperature_celsius(double *temperature);
  * @returns 0 on success, non-zero error code on failure
  */
 int read_temperature_fahrenheit(double *temperature);
+
+
